a90b221ac16874f0266a60d82363299b163955ee
bjonesmroots <bjones.mroos@gmail.com>
2020-07-31 19:06:46 -0300

    3 jugadores

--- cuatro_en_linea/app.js
+++ cuatro_en_linea/app.js
@@ -4,6 +4,7 @@ var contexto;
 var jugadorActual;
 var fichaRoja;
 var fichaAmarilla;
+var fichaNegra;
 var labelTurno;
 var fichasTotales = 0;
 var juegoTerminado = false;
@@ -12,11 +13,19 @@ var partidas;
 var botonGuardar;
 var tiempoRojo = 0;
 var tiempoAmarillo = 0;
+var tiempoNegro = 0;
 var lblTiempoRojo;
 var lblTiempoAmarillo;
+var lblTiempoNegro;
+var contenedorTiempoNegro;
+var cantidadJugadores = 2;
+var nombreJugadores = [];
+
 window.onload = function () {    
     lblTiempoRojo = document.getElementById("tiempoRojo");
     lblTiempoAmarillo = document.getElementById("tiempoAmarillo");
+    lblTiempoNegro = document.getElementById("tiempoNegro");
+    contenedorTiempoNegro = document.getElementById("contenedorTiempoNegro");
     botonGuardar = document.getElementById("guardar_partida");
     canvas = document.getElementById("tablero");
     labelTurno = document.getElementById("labelTurno");
@@ -35,19 +44,41 @@ window.onload = function () {
                     calcularTurno();
                 }, 50);
             }
-            localStorage.setItem('tablero', JSON.stringify(tablero));
-            localStorage.setItem('jugadorActual', jugadorActual);
-            localStorage.setItem('tiempoRojo', tiempoRojo);
-            localStorage.setItem('tiempoAmarillo', tiempoAmarillo);
+            guardarLocalStorage();
         } else {
             alert('El juego a terminado, por favor reinicie.');
         }
     }, false);
-    iniciarJuego();
+    cargarLocalStorage();
+    if (!cantidadJugadores) {
+        solicitarNombres();
+    } else {
+        iniciarJuego();
+    }
     cargarResultados();
     cargarPartidasGuardadas();
-    contador();
 }
+
+function solicitarNombres() {
+    juegoTerminado = true;
+    var modal2 = document.getElementById("myModal2");
+    modal2.style.display = "block";
+    var form = this.document.getElementById("form");
+    form.onsubmit = function(event) {
+      event.preventDefault();
+      nombreJugadores = new Array();  
+      cantidadJugadores = parseInt(form.elements["cantidad_jugadores"].value);
+      nombreJugadores.push(form.elements["nombre_rojo"].value);
+      nombreJugadores.push(form.elements["nombre_amarillo"].value);
+      nombreJugadores.push(form.elements["nombre_negro"].value);
+      localStorage.setItem('nombreJugadores', JSON.stringify(nombreJugadores));
+      modal2.style.display = "none";
+      labelTurno.innerHTML = "Turno del jugador Rojo (" + nombreJugadores[0] + ")";
+      juegoTerminado = false;
+      iniciarJuego();
+    }
+}
+
 
 function cargarResultados() {    
     resultados = localStorage.getItem('resultados');
@@ -69,35 +100,43 @@ function cargarPartidasGuardadas() {
     armarTablaPartidas(partidas);
 }
 
-function iniciarJuego() {
+function iniciarJuego() {    
+    if (cantidadJugadores == 3) {
+        contenedorTiempoNegro.style.display = 'block'; 
+    } else {
+        contenedorTiempoNegro.style.display = 'none';
+    }
     limpiarTablero();
     jugadorActual = 1;
     tiempoRojo = 0;
     tiempoAmarillo = 0;
+    tiempoNegro = 0;
+    guardarLocalStorage();
 }
 
 function contador() {
-    if (!juegoTerminado) {
+    if (!juegoTerminado && nombreJugadores.length > 0) {
         if (jugadorActual == 1) {
             tiempoRojo += 1;
-        } else {
+        } else if (jugadorActual == 2) {
             tiempoAmarillo += 1;
-        } 
-        lblTiempoAmarillo.innerHTML = 'Amarillo: ' + tiempoAmarillo + ' segundos';
-        lblTiempoRojo.innerHTML = 'Rojo: ' + tiempoRojo + ' segundos';
+        } else {
+            tiempoNegro += 1;
+        }
+        lblTiempoRojo.innerHTML = nombreJugadores[0] + ': ' + tiempoRojo + ' segundos';
+        lblTiempoAmarillo.innerHTML = nombreJugadores[1] + ': ' + tiempoAmarillo + ' segundos';
+        lblTiempoNegro.innerHTML = nombreJugadores[2] + ': ' + tiempoNegro + ' segundos';
         setTimeout(contador, 1000);
     }
 }
 
-function reiniciarJuego() {
-    const reiniciarContador = juegoTerminado;
+function reiniciarJuego(solicitarNom) {
     juegoTerminado = false;
-    if (reiniciarContador) {
-        contador();
+    if (solicitarNom) {
+        solicitarNombres();
     }
     botonGuardar.style.display = 'inline'; 
     localStorage.removeItem('tablero');
-    iniciarJuego();
 }
 
 
@@ -105,10 +144,14 @@ function limpiarTablero() {
     fichasTotales = 0;
     contexto.clearRect(0, 0, canvas.width, canvas.height);
     tablero = new Array();
-    for(var i=0; i<7; i++){
+    for(var i=0; i<10; i++){
+        if (cantidadJugadores == 3 || i < 7) {
         tablero[i] = new Array();
-        for(var j=0; j<6; j++){
-            tablero[i].push(0);
+            for(var j=0; j<9; j++){
+                if (cantidadJugadores == 3 || i < 6) {
+                    tablero[i].push(0);
+                }
+            }
         }
     }
     this.cargarFondoYFichas(contexto);
@@ -119,8 +162,18 @@ function cargarFondoYFichas() {
     fichaRoja.src = "imgs/roja.png";
     fichaAmarilla = new Image();
     fichaAmarilla.src = "imgs/amarilla.png";
+    fichaNegra = new Image();
+    fichaNegra.src = "imgs/negra.png";
     var background = new Image();
-    background.src = "imgs/background.png";
+    if (cantidadJugadores == 2) {
+        background.src = "imgs/background.png";
+        canvas.height = 518;
+        canvas.width = 598;
+    } else {
+        background.src = "imgs/background_3.png";
+        canvas.height = 592;
+        canvas.width = 840;
+    }
     background.onload = function(){
         contexto.drawImage(background,0,0);   
         cargarLocalStorage();
@@ -129,28 +182,41 @@ function cargarFondoYFichas() {
 
 function cargarLocalStorage() {
     var tableroLocalStorage = localStorage.getItem('tablero');
-        if (tableroLocalStorage) {
-            var tableroTemp = JSON.parse(tableroLocalStorage);
-            for(var i=6; i>=0; i--){
-                for(var j=5; j>=0; j--){
-                    jugadorActual = tableroTemp[i][j];
-                    if (jugadorActual != 0) {
-                        dibujarFicha(i,j);
+    if (tableroLocalStorage) {
+        cantidadJugadores = parseInt(localStorage.getItem('cantidadJugadores'));
+        var tableroTemp = JSON.parse(tableroLocalStorage);
+        for(var i=9; i>=0; i--){
+            if (i <=6 || cantidadJugadores == 3) {
+                for(var j=6; j>=0; j--){
+                    if (i <=5 || cantidadJugadores == 3) {
+                        jugadorActual = tableroTemp[i][j];
+                        if (jugadorActual != 0) {
+                            dibujarFicha(i,j);
+                        }
                     }
                 }
             }
-            jugadorActual = localStorage.getItem('jugadorActual');
-            tiempoAmarillo = parseInt(localStorage.getItem('tiempoAmarillo'));
-            tiempoRojo = parseInt(localStorage.getItem('tiempoRojo'));
-            calcularVictoria(false);
-            calcularTurno();
         }
+        jugadorActual = localStorage.getItem('jugadorActual');
+        tiempoAmarillo = parseInt(localStorage.getItem('tiempoAmarillo'));
+        tiempoRojo = parseInt(localStorage.getItem('tiempoRojo'));
+        tiempoNegro = parseInt(localStorage.getItem('tiempoNegro'));
+        nombreJugadores = JSON.parse(localStorage.getItem('nombreJugadores'));
+        calcularVictoria(false);
+        calcularTurno();
+        juegoTerminado = false;    
+        contador();
+    } else {        
+        cantidadJugadores = parseInt(localStorage.getItem('cantidadJugadores'));
+    }
 }
 
 function dibujarFicha(x,y) {
     var ficha = fichaRoja;
-    if (jugadorActual != 1) {
+    if (jugadorActual == 2) {
         ficha = fichaAmarilla;
+    } else if (jugadorActual == 3) {
+        ficha = fichaNegra;
     }
     if (tablero[x][y] == 0) {
         y = calcularYLibre(x);
@@ -165,20 +231,30 @@ function dibujarFicha(x,y) {
 }
 
 function calcularYLibre(x) {
-    for(var j=5; j>=0; j--){
-        if (tablero[x][j] == 0) {
-            return j;
+    for(var j=6; j>=0; j--){
+        if (j <= 5 || cantidadJugadores == 3) {
+            if (tablero[x][j] == 0) {
+                return j;
+            }
         }
     }
 }
 
-function calcularTurno() {
+function calcularTurno() {    
+    if (cantidadJugadores == 3) {
+        contenedorTiempoNegro.style.display = 'block'; 
+    } else {
+        contenedorTiempoNegro.style.display = 'none';
+    }
     if (jugadorActual == 1) {
         jugadorActual = 2;
-        labelTurno.innerHTML = "Turno del jugador Amarillo";
-    } else {
+        labelTurno.innerHTML = "Turno del jugador Amarillo (" + nombreJugadores[1] + ")";
+    } else if (jugadorActual == 2 && cantidadJugadores == 3) {
+        jugadorActual = 3;
+        labelTurno.innerHTML = "Turno del jugador Negro (" + nombreJugadores[2] + ")";
+    } else  {
         jugadorActual = 1;
-        labelTurno.innerHTML = "Turno del jugador Rojo";
+        labelTurno.innerHTML = "Turno del jugador Rojo (" + nombreJugadores[0] + ")";
     }
 }
 
@@ -205,7 +281,12 @@ function calcularVictoria(guardar) {
     
     if (victoria) {        
         juegoTerminado = true;
-        var ganador = (jugadorActual == 1 ? 'Rojo' : 'Amarillo');
+        var ganador = 'Rojo';
+        if (jugadorActual == 2) {
+            ganador = 'Amarillo';
+        } else if (jugadorActual == 3) {
+            ganador = 'Negro';
+        }
         dibujarTextoVictoria(ganador);
         if (guardar) {
             guardarResultados({
@@ -288,14 +369,14 @@ function calcularVictoriaAb(x,y) {
 
 function calcularVictoriaAbI(x,y) {
     var esIgual = true;
-    var contador = 0;
+    var cont = 0;
     for(var i=y; i<y+4; i++){
-        var j = x-contador;
+        var j = x-cont;
         if (i > 5 || i < 0 || j > 6 || j < 0 || tablero[j][i] != jugadorActual) {
             esIgual = false;
             break;
         }
-        contador++;
+        cont++;
     }
     if (esIgual) {
         dibujarLineaVictoria(x*85+42.5,y*85+42.5,(x-3)*85+42.5,(y+3)*85+42.5);
@@ -305,14 +386,14 @@ function calcularVictoriaAbI(x,y) {
 
 function calcularVictoriaAbD(x,y) {
     var esIgual = true;
-    var contador = 0;
+    var cont = 0;
     for(var i=y; i<y+4; i++){
-        var j = x+contador;
+        var j = x+cont;
         if (i > 5 || i < 0 || j > 6 || j < 0 || tablero[j][i] != jugadorActual) {
             esIgual = false;
             break;
         }
-        contador++;
+        cont++;
     }
     if (esIgual) {
         dibujarLineaVictoria(x*85+42.5,y*85+42.5,(x+3)*85+42.5,(y+3)*85+42.5);
@@ -323,46 +404,48 @@ function calcularVictoriaAbD(x,y) {
 
 
 function armarTabla(resultados) {
-    var col = [];
-    col.push(['Fecha','fecha']);
-    col.push(['Resultado','resultado']);
-    col.push(['Captura','imagen']);
-  
-    var table = document.createElement("table");
-  
-    var tr = table.insertRow(-1);
-  
-    for (var i = 0; i < col.length; i++) {
-        var th = document.createElement("th");
-        th.innerHTML = col[i][0];
-        tr.appendChild(th);
-    }
-  
-    for (var i = resultados.length - 1; i >= 0; i--) {  
-        tr = table.insertRow(-1);  
-        for (var j = 0; j < col.length; j++) {
-            var tabCell = tr.insertCell(-1);
-            var valor = resultados[i][col[j][1]];
-            if (col[j][1] == 'imagen') {
-                var img = document.createElement("img");
-                img.src = valor;
-                img.width = 100;
-                img.alt = resultados[i][col[1][1]];
-                img.className = "imgResultado"
-                tabCell.appendChild(img);
-            } else {
-                tabCell.innerHTML = valor;
+    if (resultados.length > 0) {
+        var col = [];
+        col.push(['Fecha','fecha']);
+        col.push(['Resultado','resultado']);
+        col.push(['Captura','imagen']);
+    
+        var table = document.createElement("table");
+    
+        var tr = table.insertRow(-1);
+    
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");
+            th.innerHTML = col[i][0];
+            tr.appendChild(th);
+        }
+    
+        for (var i = resultados.length - 1; i >= 0; i--) {  
+            tr = table.insertRow(-1);  
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                var valor = resultados[i][col[j][1]];
+                if (col[j][1] == 'imagen') {
+                    var img = document.createElement("img");
+                    img.src = valor;
+                    img.width = 100;
+                    img.alt = resultados[i][col[1][1]];
+                    img.className = "imgResultado"
+                    tabCell.appendChild(img);
+                } else {
+                    tabCell.innerHTML = valor;
+                }
             }
         }
+    
+        var divContainer = document.getElementById("resultados");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
     }
-  
-    var divContainer = document.getElementById("resultados");
-    divContainer.innerHTML = "";
-    divContainer.appendChild(table);
 
 }
 
-function cargarModal() {
+function cargarModales() {
     var modal = document.getElementById("myModal");
 
     var imgs = document.getElementsByClassName("imgResultado");
@@ -380,6 +463,12 @@ function cargarModal() {
 
     span.onclick = function() {
         modal.style.display = "none";
+    }
+    var modal2 = document.getElementById("myModal2");
+    var span2 = document.getElementsByClassName("close")[1];
+
+    span2.onclick = function() {
+        modal2.style.display = "none";
     }
 }
 
@@ -428,12 +517,16 @@ function armarTablaPartidas(partidas) {
                     btn.innerHTML = 'Recuperar';
                     btn.value = i;
                     btn.onclick = function recuperarPartida() {
-                        reiniciarJuego();
-                        setTimeout(() => {                            
+                        reiniciarJuego(false);
+                        setTimeout(() => {            
+                            juegoTerminado = true;    
                             localStorage.setItem('tablero',JSON.stringify(partidas[this.value].tablero));
                             localStorage.setItem('jugadorActual',partidas[this.value].jugadorActual);
                             localStorage.setItem('tiempoRojo',partidas[this.value].tiempoRojo);
                             localStorage.setItem('tiempoAmarillo',partidas[this.value].tiempoAmarillo);
+                            localStorage.setItem('tiempoNegro',partidas[this.value].tiempoNegro);
+                            localStorage.setItem('cantidadJugadores',partidas[this.value].cantidadJugadores);
+                            localStorage.setItem('nombreJugadores',JSON.stringify(partidas[this.value].nombreJugadores));
                             cargarLocalStorage();
                         }, 200);
                     }
@@ -448,7 +541,7 @@ function armarTablaPartidas(partidas) {
         divContainer.innerHTML = "";
         divContainer.appendChild(table);
     }
-    cargarModal();
+    cargarModales();
 }
 
 
@@ -460,10 +553,32 @@ function guardarPartida() {
         fecha : formatDate(new Date()),
         imagen : canvas.toDataURL("image/png"),
         tablero: tablero,
-        jugadorActual: jugadorActual == 1 ? 2 : 1,
+        jugadorActual: jugadorActual == 3 ? 2 : (jugadorActual == 2 ? 1 : 3),
         tiempoRojo: tiempoRojo,
-        tiempoAmarillo: tiempoAmarillo
+        tiempoAmarillo: tiempoAmarillo,
+        tiempoNegro: tiempoNegro,
+        cantidadJugadores: cantidadJugadores,
+        nombreJugadores: nombreJugadores
     });
     localStorage.setItem('partidas', JSON.stringify(partidas));
     cargarPartidasGuardadas();
+}
+
+function ocultarMostrar3(jugadores) {
+    var jugador3 = document.getElementById("jugador3");
+    if (jugadores.value == 2) {
+        jugador3.disabled = true;
+    } else {
+        jugador3.disabled = false;
+    }
+}
+
+function guardarLocalStorage() {    
+    localStorage.setItem('tablero', JSON.stringify(tablero));
+    localStorage.setItem('jugadorActual', jugadorActual);
+    localStorage.setItem('tiempoRojo', tiempoRojo);
+    localStorage.setItem('tiempoAmarillo', tiempoAmarillo);
+    localStorage.setItem('tiempoNegro', tiempoNegro);
+    localStorage.setItem('cantidadJugadores', cantidadJugadores);
+    localStorage.setItem('nombreJugadores', JSON.stringify(nombreJugadores));
 }
